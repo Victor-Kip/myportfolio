b42469f3d5b439ee8b85c16c86203a28d6cc99de
Victor-Kip <vicklan132@gmail.com>
2025-01-30 15:08:25 +0300

    add header logic

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>My Portfolio</h1>
+      <ul>
+        <li>
+          <a href="#about">About Me</a>
+        </li>
+        <li>
+          <a href="#projects">Projects</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+    </header>
+  );
+}
+export default Header;
