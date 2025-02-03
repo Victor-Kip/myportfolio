1f84dcc86139ee457d54b26af89ea68e37e76684
Victor-Kip <vicklan132@gmail.com>
2025-02-03 09:43:45 +0300

    update picture

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,9 @@
 import React from "react";
-import victorImage from '../pictures/victorlangat.jpg';
-
 
 function Hero() {
   return (
     <>
     <div className="hero">
-        <img src = {victorImage} alt = "Victor Langat"/>
         <div className="hero-text">
       <h2>Victor Kipngetich Langat</h2>
       <ul>
