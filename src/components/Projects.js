160cc15942f35d254581cf12b0b9e58d239fd839
Victor-Kip <vicklan132@gmail.com>
2025-02-04 15:47:13 +0300

    add project screenshots

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,6 @@
 import React from "react";
+import nairouteImg from "../pictures/dash.png";
+import honeymedsImg from "../pictures/honeymeds.png";
 
 function Projects() {
     return (
@@ -7,11 +9,11 @@ function Projects() {
         <h2>HoneyMeds</h2>
         <div>
         <p>A web based pharmacy built with PHP and MySQL that connects doctors,patients, and pharmaceutical companies. It streamlines prescription management, mediacation access and pharmaceutical transactions for a seamless healthcare experience.</p>
-        <img src="https://res.cloudinary.com/dk" alt = 'HoneyMeds'/ >
+        <img src={honeymedsImg} alt = 'HoneyMeds'/ >
          </div>
         <h2>Nairoute</h2>
         <div>
-        <img src="https://res.cloudinary.com/dk" alt = 'Nairoute'/ >
+        <img src={nairouteImg} alt = 'Nairoute'/ >
         <p>A smart public transport solution for Nairobi developed using React and Express. Inspired by Uber, Nairoute connects commuters with public transport options. It provides real-time tracking,route optimization and a seamless booking experience</p>
         </div>
         <h2>My Portfolio</h2>
