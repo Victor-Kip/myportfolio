2755577631f04090187d41e6ad6694b0df6d5eac
Victor-Kip <vicklan132@gmail.com>
2025-02-11 10:17:12 +0300

    mobify contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,24 +8,31 @@ function Contact() {
   return (
     <div>
       <h1>Contact Me</h1>
+      <p>Feel free to contact me for any queries</p>
+      <div className='contacts'>
+      <div>
       <ul>
         <li><strong>Email: </strong>vicklan132@gmail.com</li>
         <li><strong>Phone No:</strong>0710414911</li>
       </ul>
+      </div>
+      <div style={{marginLeft: 'auto',marginRight: 'auto'}}>
       <h2>Leave a message?</h2>
       <form onSubmit={hello}>
         <p>
         <label for = 'fullName: '>Full Name</label>
-        <input type='text'id = 'fullName' ></input>
+        <input type='text' id = 'fullName' name = 'fullName' ></input>
         </p>
         <p>
           <label for = 'message'>Message: </label>
         </p>
         <p>
-          <input type='text' id = 'message'></input>
+          <textarea id = 'message' name = 'message' rows={5} cols={50}></textarea>
         </p>
         <input type='submit' id = 'submitButton'/>
       </form>
+      </div>
+      </div>
     </div>
   );
 }
