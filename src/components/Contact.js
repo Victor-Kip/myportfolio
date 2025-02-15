b380bff2cc13cc458962ad5bb24e4d83a3620528
Victor-Kip <vicklan132@gmail.com>
2025-02-15 12:49:52 +0300

    add feedback record

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 function Contact() {
+  const [hasFeedback,setHasFeedback] = useState(false);
+  const [feedMessage,setFeedMessage] = useState("");
+  const [feedFullName,setFeedFullName] = useState("")
 
-  function hello(){
-    alert('Hello there')
+  function writeFeedback(){
+    if (feedMessage !== ''){
+      setHasFeedback(true)
+    }
   }
   return (
     <div>
@@ -16,23 +21,30 @@ function Contact() {
         <li><strong>Phone No:</strong>0710414911</li>
       </ul>
       </div>
-      <div style={{marginLeft: 'auto',marginRight: 'auto'}}>
+      <div className = "message">
       <h2>Leave a message?</h2>
-      <form onSubmit={hello}>
+      <form onSubmit={writeFeedback}>
         <p>
         <label for = 'fullName: '>Full Name</label>
-        <input type='text' id = 'fullName' name = 'fullName' ></input>
+        <input type='text' id = 'fullName' name = 'fullName' onChange={(e)=>setFeedFullName(e.target.value)} ></input>
         </p>
         <p>
           <label for = 'message'>Message: </label>
         </p>
         <p>
-          <textarea id = 'message' name = 'message' rows={5} cols={50}></textarea>
+          <textarea id = 'message' name = 'message' rows={5} cols={50} onChange={(e)=>setFeedMessage(e.target.value)}></textarea>
         </p>
         <input type='submit' id = 'submitButton'/>
       </form>
       </div>
       </div>
+      <h3>Feedback</h3>
+      {hasFeedback && (
+        <div>
+        <p>{feedFullName}</p>
+        <p>feedMessage</p>
+        </div>
+      )}
     </div>
   );
 }
